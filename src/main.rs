72cac2bbe33aff903c1f92727d0c628fa0a65a44
banana <jgjin@umich.edu>
2019-03-05 13:16:31 -0500

    album_crawl main

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate serde;
 extern crate serde_json;
 
 mod album;
+mod album_crawl;
 mod album_types;
 mod artist;
 mod artist_crawl;
@@ -25,6 +26,7 @@ use std::{
         Arc,
         RwLock,
     },
+    thread,
 };
 
 use crossbeam_channel as channel;
@@ -42,26 +44,49 @@ fn main(
             .access_token
     ));
 
-    let seed_artists: Vec<String> = io::lines_from_file("seed_artists.txt")
-        .expect("Error in reading file")
-        .into_iter().map(|query| {
-            artist::search_artists(client.clone(), token.clone(), &query[..])
-                .expect("Error in artist::search_artists")
-                .items[0].id.to_owned()
-        }).collect();
-
-
-    let (sender, receiver) = channel::unbounded();
-    let limit = 100000;
-
-    artist_crawl::artist_crawl(
-        seed_artists.iter().map(|artist_id| &artist_id[..]).collect(),
-        limit,
-        client,
-        token,
-        sender,
-    );
+    let (artist_sender, artist_receiver) = channel::unbounded();
+    let (album_sender, album_receiver) = channel::unbounded();
     
-    io::write_csv_through_receiver(receiver, limit, "artists_crawled.csv")
-        .expect("Error in writing csv");
+
+    let reader_thread = thread::spawn(move || {
+        io::read_csv_into_sender::<artist_types::ArtistCsv>(
+            artist_sender,
+            "artists_crawled.csv",
+        ).unwrap_or_else(|err| {
+            println!("Error in reading csv: {}", err)
+        });
+        // println!("Finished reading");
+    });
+
+    let crawler_thread = thread::spawn(move || {
+        album_crawl::album_crawl(
+            artist_receiver,
+            client,
+            token,
+            album_sender
+        ).unwrap_or_else(|err| {
+            println!("Error in crawling albums: {:?}", err)
+        });
+        // println!("Finished crawling");
+    });
+
+    let writer_thread = thread::spawn(move || {
+        io::write_csv_through_receiver(album_receiver, "albums_crawled.csv")
+            .unwrap_or_else(|err| {
+                println!("Error in writing csv: {}", err)
+            });
+        // println!("Finished writing");
+    });
+
+    reader_thread.join().unwrap_or_else(|err| {
+        println!("Error in csv reader thread: {:?}", err);
+    });
+
+    crawler_thread.join().unwrap_or_else(|err| {
+        println!("Error in album crawler thread: {:?}", err);
+    });
+
+    writer_thread.join().unwrap_or_else(|err| {
+        println!("Error in csv writer thread: {:?}", err);
+    });
 }
