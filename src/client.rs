8d8dbac8f2d766ca5a05a06c100b382400be8312
banana <jgjin@umich.edu>
2019-03-17 21:36:51 -0400

    * src/client.rs: impl clientring

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,15 @@
 use std::{
+    clone::{
+        Clone,
+    },
     collections::{
         HashMap,
+    },
+    error::{
+        Error,
+    },
+    net::{
+        Ipv4Addr,
     },
     sync::{
         Arc,
@@ -18,6 +27,7 @@ use atomicring::{
     AtomicRingQueue,
 };
 use reqwest::{
+    self,
     Client,
 };
 use serde::{
@@ -39,133 +49,135 @@ struct AccessToken {
     pub scope: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct SpotifyClient {
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct SpotifyClientMetadata {
     pub name: String,
     pub id: String,
     pub secret: String,
 }
 
-#[derive(Debug)]
-struct SpotifyClientWithToken {
+#[derive(Clone, Debug)]
+struct SpotifyClientWithProxy {
+    pub client_metadata: SpotifyClientMetadata,
+    pub client: Client,
+    pub proxy: Proxy,
     pub token: String,
-    pub client: SpotifyClient,
 }
 
-impl SpotifyClientWithToken {
-    pub fn new(
-        token: String,
-        client: SpotifyClient,
-    ) -> Self {
-        Self {
-            token: token,
+impl SpotifyClientWithProxy {
+    pub fn init(
+        client_metadata: SpotifyClientMetadata,
+        proxy: Proxy,
+    ) -> reqwest::Result<Self> {
+        let client = Client::builder()
+            .proxy(reqwest::Proxy::all(&format!("{}:{}", proxy.ip_address, proxy.port)[..])?)
+            .build()?;
+
+        let token = retrieve_access_token(
+            &client,
+            &client_metadata.id[..],
+            &client_metadata.secret[..],
+        )?.access_token;
+        
+        Ok(Self {
+            client_metadata: client_metadata,
             client: client,
-        }
+            proxy: proxy,
+            token: token,
+        })
     }
 }
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Proxy {
+    pub ip_address: Ipv4Addr,
+    pub port: u16,
+}
+
 pub struct ClientRing {
-    current_token: SpotifyClientWithToken,
-    ring: Arc<AtomicRingQueue<SpotifyClientWithToken>>,
-    client: Arc<Client>,
+    current_client: SpotifyClientWithProxy,
+    client_ring: Arc<AtomicRingQueue<SpotifyClientWithProxy>>,
+    proxies: Arc<AtomicRingQueue<Proxy>>,
 }
 
 impl ClientRing {
     pub fn init(
-        client: Arc<Client>,
-    ) -> Self {
-        let mut tokens: Vec<SpotifyClientWithToken> = structs_from_file::<SpotifyClient>("clients.csv")
-            .expect("Error in reading Spotify clients")
-            .into_iter().map(|spotify_client| {
-                SpotifyClientWithToken::new(
-                    retrieve_access_token(
-                        client.clone(),
-                        &spotify_client.id[..],
-                        &spotify_client.secret[..],
-                    ).expect("Error in retrieving Spotify API tokens").access_token,
-                    spotify_client,
-                )
-            }).collect();
+    ) -> Result<Self, Box<dyn Error>> {
+        let clients_metadata = structs_from_file::<SpotifyClientMetadata>("clients.csv")?;
+        let proxies = structs_from_file::<Proxy>("proxies.csv")?;
 
-        let token_ring = Self {
-            // Segfaults when running (potential crate issue), fix with extra slots
-            // Extra slots for each token in case pops take too long relative to pushes
-            ring: Arc::new(AtomicRingQueue::with_capacity(tokens.len() * 2)),
-            current_token: tokens.pop().expect("Empty Spotify clients"),
-            client: client,
-        };
+        let client_metadata_len = clients_metadata.len();
+        let mut clients_with_proxies = clients_metadata.into_iter().zip(
+            proxies.iter().cloned().cycle().take(client_metadata_len),
+        ).map(|(client_metadata, proxy)| {
+            SpotifyClientWithProxy::init(client_metadata, proxy)
+        }).collect::<reqwest::Result<Vec<SpotifyClientWithProxy>>>()?;
 
-        tokens.into_iter().map(|spotify_client_with_token| {
-            // Use push_overwrite because guaranteed enough space in queue
-            token_ring.ring.push_overwrite(spotify_client_with_token);
+        let current_client = clients_with_proxies.pop().expect("Empty clients or proxies");
+        let client_ring = Arc::new(AtomicRingQueue::with_capacity(client_metadata_len * 2));
+        clients_with_proxies.into_iter().map(|client_with_proxy| {
+            client_ring.push_overwrite(client_with_proxy);
         }).last();
+        
+        let proxies_queue = Arc::new(AtomicRingQueue::with_capacity(
+            (client_metadata_len + proxies.len()) * 2,
+        ));
+        proxies.into_iter().map(|proxy| { proxies_queue.push_overwrite(proxy); }).last();
 
-        token_ring
+        Ok(Self {
+            current_client: current_client,
+            client_ring: client_ring,
+            proxies: proxies_queue,
+        })
     }
 
     pub fn front(
         &self,
-    ) -> &String {
-        &self.current_token.token
+    ) -> (Client, String) {
+        (self.current_client.client.clone(), self.current_client.token.clone())
     }
 
     pub fn sleep_front_and_get_next(
         &mut self,
         secs: u64,
     ) {
-        info!("Sleeping {} token {} seconds", self.current_token.client.name, secs);
+        info!("Sleeping {} client {} seconds", self.current_client.client_metadata.name, secs);
 
-        let ring_clone = self.ring.clone();
-        let new_token = SpotifyClientWithToken::new(
-            self.current_token.token.clone(),
-            SpotifyClient {
-                name: self.current_token.client.name.clone(),
-                id: self.current_token.client.id.clone(),
-                secret: self.current_token.client.secret.clone(),
-            },
-        );
+        let ring_clone = self.client_ring.clone();
+        let current_client_clone = self.current_client.clone();
         thread::spawn(move || {
             sleep(Duration::from_secs(secs));
-            ring_clone.try_push(new_token).expect("Error in pushing new token in sleep");
+            ring_clone.try_push(current_client_clone)
+                .expect("Error in pushing new client in sleep");
         });
 
-        self.current_token = self.ring.pop();
-        info!("Using {} token", self.current_token.client.name);
+        self.current_client = self.client_ring.pop();
+        info!("Using {} client", self.current_client.client_metadata.name);
     }
 
     pub fn refresh_front_and_get_next(
         &mut self,
     ) {
-        info!("Refreshing {} token", self.current_token.client.name);
-        
-        self.ring.try_push(SpotifyClientWithToken::new(
-            retrieve_access_token(
-                self.client.clone(),
-                &self.current_token.client.id[..],
-                &self.current_token.client.secret[..],
-            ).unwrap_or(AccessToken{
-                access_token: self.current_token.token.clone(),
-                token_type: String::new(),
-                expires_in: 0,
-                scope: String::new(),
-            }).access_token,
-            SpotifyClient {
-                name: self.current_token.client.name.clone(),
-                id: self.current_token.client.id.clone(),
-                secret: self.current_token.client.secret.clone(),
-            },
-        )).expect("Error in pushing new token in refresh");
-        
-        self.current_token = self.ring.pop();
-        info!("Using {} token", self.current_token.client.name);
+        info!("Refreshing {} client", self.current_client.client_metadata.name);
+
+        self.proxies.try_push(self.current_client.proxy.clone())
+            .expect("Error in pushing new proxy in refresh");
+
+        self.client_ring.try_push(SpotifyClientWithProxy::init(
+            self.current_client.client_metadata.clone(),
+            self.proxies.pop(),
+        ).expect("Error in refreshing client")).expect("Error in pushing new client in refresh");
+
+        self.current_client = self.client_ring.pop();
+        info!("Using {} client", self.current_client.client_metadata.name);
     }
 }
 
 fn retrieve_access_token(
-    client: Arc<Client>,
+    client: &Client,
     id: &str,
     secret: &str,
-) -> Result<AccessToken, reqwest::Error> {
+) -> reqwest::Result<AccessToken> {
     let mut form_data = HashMap::new();
     form_data.insert("grant_type", "client_credentials");
 
